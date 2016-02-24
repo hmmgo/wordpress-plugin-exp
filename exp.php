5118086f9f7168a5396ef511705175e55ebf8008
hmm007 <hmmpython@gmail.com>
2016-02-24 16:07:56 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -2,8 +2,14 @@
 //experimental plugin do not install in production
 
 class  exp {
+//should be static as codex
      static function install() {
+            // timely written code
+     }
+     
+          static function uninstall() {
             // timely written code
      }
 }
 register_activation_hook( __FILE__, array( 'exp', 'install' ) );
+register_deactivation_hook( __FILE__, array( 'exp', 'uninstall' ) );
