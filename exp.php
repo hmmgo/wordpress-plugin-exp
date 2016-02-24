5e97501fcbf0d22309862822dbb1d711f0e85e9f
hmm007 <hmmpython@gmail.com>
2016-02-24 15:58:38 +0100

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,1 +1,9 @@
 
+//experimental plugin do not install in production
+
+class  exp {
+     static function install() {
+            // timely written code
+     }
+}
+register_activation_hook( __FILE__, array( 'exp', 'install' ) );
